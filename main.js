98fefee11d3d03fb792bff6701331fa597f8398a
Omotolani Afolayan <afolayanomotolani30@gmail.com>
2024-08-28 14:35:40 +0100

    my commits

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-alert("You are welcome today");
+document.addEventListener("DOMContentLoaded", function() {
+    const currentPage = location.pathname.split("/").pop(); // Get the current page filename
+    const navLinks = document.querySelectorAll(".navbar .nav-link");
+
+    navLinks.forEach(link => {
+        if (link.getAttribute("href") === currentPage) {
+            link.classList.add("active");
+        }
+    });
+});
